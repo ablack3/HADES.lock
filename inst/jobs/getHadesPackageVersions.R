083bc20cb66784fc6118d8392b0699eb7fb87d27
Adam Black <ablack3@gmail.com>
2021-05-27 17:01:03 -0400

    troubleshoot rJava

--- inst/jobs/getHadesPackageVersions.R
+++ inst/jobs/getHadesPackageVersions.R
@@ -1,20 +1,21 @@
-library(httr)
-library(purrr)
-
-# order matters here
-hadesPackages <- c("Andromeda","SqlRender", "DatabaseConnector", "BigKnn","CirceR","CohortDiagnostics","CohortMethod","Cyclops","EmpiricalCalibration","Eunomia","EvidenceSynthesis","FeatureExtraction","Hydra","MethodEvaluation","OhdsiSharing","ParallelLogger","PatientLevelPrediction","ROhdsiWebApi","SelfControlledCaseSeries","SelfControlledCohort")
-
-getHadesPackageVersion <- function(packageName){
-  r <- httr::GET(glue::glue("https://api.github.com/repos/OHDSI/{packageName}/tags"))
-  httr::stop_for_status(r)
-  httr::content(r)[[1]]$name
-}
-
-# getHadesPackageVersion(hadesPackages[[2]])
-
-hadesPackageVersions <- purrr::map_chr(hadesPackages, getHadesPackageVersion)
-
-df <- data.frame(package = hadesPackages, version = hadesPackageVersions)
+library(rJava)
+# library(httr)
+# library(purrr)
+# 
+# # order matters here
+# hadesPackages <- c("Andromeda","SqlRender", "DatabaseConnector", "BigKnn","CirceR","CohortDiagnostics","CohortMethod","Cyclops","EmpiricalCalibration","Eunomia","EvidenceSynthesis","FeatureExtraction","Hydra","MethodEvaluation","OhdsiSharing","ParallelLogger","PatientLevelPrediction","ROhdsiWebApi","SelfControlledCaseSeries","SelfControlledCohort")
+# 
+# getHadesPackageVersion <- function(packageName){
+#   r <- httr::GET(glue::glue("https://api.github.com/repos/OHDSI/{packageName}/tags"))
+#   httr::stop_for_status(r)
+#   httr::content(r)[[1]]$name
+# }
+# 
+# # getHadesPackageVersion(hadesPackages[[2]])
+# 
+# hadesPackageVersions <- purrr::map_chr(hadesPackages, getHadesPackageVersion)
+# 
+# df <- data.frame(package = hadesPackages, version = hadesPackageVersions)
 
 # read latest version file
 # sort(list.files("versions"))[[1]]
@@ -26,18 +27,18 @@ write.csv(df,
           row.names = FALSE)
 
 
-library(renv)
-library(remotes)
-renv::init()
-remotes::install_github("OHDSI/Hades", dependencies = TRUE, upgrade = "always")
+# library(renv)
+# library(remotes)
+# renv::init()
+# remotes::install_github("OHDSI/Hades", dependencies = TRUE, upgrade = "always")
 
 # purrr::walk2(df$package, df$version, 
 #              ~install_github(glue::glue("OHDSI/{.x}"),
 #                                       ref = .y,
 #                                       dependencies = TRUE, 
 #                                       upgrade = "always"))
-renv::snapshot()
+# renv::snapshot()
 
-file.copy("renv.lock", "./inst/lockfiles/{Sys.Date()}-HADES-renv.lock")
+# file.copy("renv.lock", "./inst/lockfiles/{Sys.Date()}-HADES-renv.lock")
 
 
